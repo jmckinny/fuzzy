b20809aecf42472e1316f8c1dd2f160f077a3871
jmckinny <justinmckinney.md@gmail.com>
2022-11-08 12:42:40 -0500

    fix: improper indexing of strings

--- src/main.rs
+++ src/main.rs
@@ -37,23 +37,38 @@ fn search_dir(path: &Path, target: &str) -> Result<Vec<hit::Hit>, std::io::Error
 }
 
 fn levenshtein_distance(target: &String, other: &String) -> usize {
-    if target.is_empty() {
-        other.len()
-    } else if other.is_empty() {
-        target.len()
-    } else if target.chars().next() == other.chars().next() {
-        levenshtein_distance(
-            &target.chars().skip(1).collect(),
-            &other.chars().skip(1).collect(),
-        )
-    } else {
-        let target_tail = target.chars().skip(1).collect();
-        let other_tail = other.chars().skip(1).collect();
-        let insert = levenshtein_distance(target, &other_tail);
-        let delete = levenshtein_distance(&target_tail, other);
-        let replaced = levenshtein_distance(&target_tail, &other_tail);
-        1 + insert.min(delete.min(replaced))
+    let mut matrix = vec![vec![0_usize; target.len() + 1]; other.len() + 1];
+
+    // Empty string sub-problems can just be solved by inserting i characters
+    for i in 0..=other.len() {
+        matrix[i][0] = i;
     }
+
+    for j in 0..=target.len() {
+        matrix[0][j] = j
+    }
+
+    for j in 1..=target.len() {
+        for i in 1..=other.len() {
+            let substitue_cost =
+                if target.chars().nth(j - 1).unwrap() == other.chars().nth(i - 1).unwrap() {
+                    0
+                } else {
+                    1
+                };
+
+            matrix[i][j] = min3(
+                matrix[i - 1][j] + 1,                  //deletion
+                matrix[i][j - 1] + 1,                  //insertion
+                matrix[i - 1][j - 1] + substitue_cost, //substitution
+            );
+        }
+    }
+    matrix[other.len()][target.len()]
+}
+
+fn min3(x: usize, y: usize, z: usize) -> usize {
+    x.min(y.min(z))
 }
 
 fn load_file(path: &Path) -> Result<String, std::io::Error> {
@@ -90,7 +105,7 @@ mod tests {
     }
 
     #[test]
-    fn distance() {
+    fn basic_distance() {
         assert_eq!(
             levenshtein_distance(&"Hello".to_string(), &"Hello".to_string()),
             0
@@ -106,6 +121,22 @@ mod tests {
         assert_eq!(
             levenshtein_distance(&"Heo".to_string(), &"Hello".to_string()),
             2
+        );
+    }
+
+    #[test]
+    fn complex_distance() {
+        assert_eq!(
+            levenshtein_distance(&String::from("kitten"), &String::from("sitting")),
+            3
+        );
+        assert_eq!(
+            levenshtein_distance(&String::from("sunday"), &String::from("saturday")),
+            3
+        );
+        assert_eq!(
+            levenshtein_distance(&String::from("manual"), &String::from("=")),
+            6
         );
     }
 }
