56a0410986ceaafa9054115d8191c2c328060f3b
jmckinny <justinmckinney.md@gmail.com>
2022-11-08 13:12:26 -0500

    feature: add clap for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,142 +1,23 @@
-use std::{io::Read, path::Path};
+use std::path::Path;
 mod hit;
+mod search;
+use clap::Parser;
 
-const LIMIT: usize = 2;
 fn main() -> Result<(), std::io::Error> {
-    let args: Vec<String> = std::env::args().collect();
-    let target = args.get(1).expect("No search item given!");
-    let results = search_dir(Path::new("./"), target)?;
+    let args = Args::parse();
+    let target = args.target;
+    let results = search::search_dir(Path::new("./"), &target, args.limit)?;
     results.iter().for_each(|x| println!("{}", x));
 
     Ok(())
 }
 
-fn search_dir(path: &Path, target: &str) -> Result<Vec<hit::Hit>, std::io::Error> {
-    let mut result = Vec::new();
-    let paths = std::fs::read_dir(path)?;
-    for file in paths {
-        let file = file?;
-        if file.file_type()?.is_file() {
-            if let Ok(data) = load_file(&file.path()) {
-                for (i, line) in data.lines().enumerate() {
-                    for s in line.split_whitespace() {
-                        let diff = levenshtein_distance(&target.to_string(), &s.to_string());
-                        if diff < LIMIT {
-                            result.push(hit::Hit::new(
-                                String::from(s),
-                                file.path().display().to_string(),
-                                i + 1,
-                            ))
-                        }
-                    }
-                }
-            }
-        }
-    }
-    Ok(result)
-}
-
-fn levenshtein_distance(target: &String, other: &String) -> usize {
-    let mut matrix = vec![vec![0_usize; target.len() + 1]; other.len() + 1];
-
-    // Empty string sub-problems can just be solved by inserting i characters
-    for i in 0..=other.len() {
-        matrix[i][0] = i;
-    }
-
-    for j in 0..=target.len() {
-        matrix[0][j] = j
-    }
-
-    for j in 1..=target.len() {
-        for i in 1..=other.len() {
-            let substitue_cost =
-                if target.chars().nth(j - 1).unwrap() == other.chars().nth(i - 1).unwrap() {
-                    0
-                } else {
-                    1
-                };
-
-            matrix[i][j] = min3(
-                matrix[i - 1][j] + 1,                  //deletion
-                matrix[i][j - 1] + 1,                  //insertion
-                matrix[i - 1][j - 1] + substitue_cost, //substitution
-            );
-        }
-    }
-    matrix[other.len()][target.len()]
-}
-
-fn min3(x: usize, y: usize, z: usize) -> usize {
-    x.min(y.min(z))
-}
-
-fn load_file(path: &Path) -> Result<String, std::io::Error> {
-    let mut file = std::fs::File::open(path)?;
-    let mut result = String::new();
-    file.read_to_string(&mut result)?;
-    Ok(result)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::path::Path;
-
-    #[test]
-    fn it_works() {
-        let results = search_dir(Path::new("./tests"), "Hello").unwrap();
-        assert_eq!(format!("{}", results[0]), "./tests/hello.txt:1\tHello");
-    }
-
-    #[test]
-    fn fuzzier() {
-        let results = search_dir(Path::new("./tests"), "Hell").unwrap();
-        assert_eq!(format!("{}", results[0]), "./tests/hello.txt:1\tHello");
-
-        let results = search_dir(Path::new("./tests"), "World!").unwrap();
-        assert_eq!(format!("{}", results[0]), "./tests/hello.txt:1\tWorld!");
-    }
-
-    #[test]
-    fn cases() {
-        let results = search_dir(Path::new("./tests"), "hello").unwrap();
-        assert_eq!(format!("{}", results[0]), "./tests/hello.txt:1\tHello");
-    }
-
-    #[test]
-    fn basic_distance() {
-        assert_eq!(
-            levenshtein_distance(&"Hello".to_string(), &"Hello".to_string()),
-            0
-        );
-        assert_eq!(
-            levenshtein_distance(&"Helo".to_string(), &"Hello".to_string()),
-            1
-        );
-        assert_eq!(
-            levenshtein_distance(&"ello".to_string(), &"Hello".to_string()),
-            1
-        );
-        assert_eq!(
-            levenshtein_distance(&"Heo".to_string(), &"Hello".to_string()),
-            2
-        );
-    }
-
-    #[test]
-    fn complex_distance() {
-        assert_eq!(
-            levenshtein_distance(&String::from("kitten"), &String::from("sitting")),
-            3
-        );
-        assert_eq!(
-            levenshtein_distance(&String::from("sunday"), &String::from("saturday")),
-            3
-        );
-        assert_eq!(
-            levenshtein_distance(&String::from("manual"), &String::from("=")),
-            6
-        );
-    }
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// String to fuzzy search for
+    target: String,
+    /// Maximum acceptable character difference for the search
+    #[arg(short, long, default_value_t = 2)]
+    limit: usize,
 }
---
+++ src/search.rs
@@ -0,0 +1,137 @@
+use crate::hit;
+use std::{io::Read, path::Path};
+
+pub fn search_dir(
+    path: &Path,
+    target: &str,
+    diff_limit: usize,
+) -> Result<Vec<hit::Hit>, std::io::Error> {
+    let mut result = Vec::new();
+    let paths = std::fs::read_dir(path)?;
+    for file in paths {
+        let file = file?;
+        if file.file_type()?.is_file() {
+            if let Ok(data) = load_file(&file.path()) {
+                for (i, line) in data.lines().enumerate() {
+                    for s in line.split_whitespace() {
+                        let diff = levenshtein_distance(&target.to_string(), &s.to_string());
+                        if diff < diff_limit {
+                            result.push(hit::Hit::new(
+                                String::from(s),
+                                file.path().display().to_string(),
+                                i + 1,
+                            ))
+                        }
+                    }
+                }
+            }
+        }
+    }
+    Ok(result)
+}
+
+fn levenshtein_distance(target: &String, other: &String) -> usize {
+    let mut matrix = vec![vec![0_usize; target.len() + 1]; other.len() + 1];
+
+    // Empty string sub-problems can just be solved by inserting i characters
+    for i in 0..=other.len() {
+        matrix[i][0] = i;
+    }
+
+    for j in 0..=target.len() {
+        matrix[0][j] = j
+    }
+
+    for j in 1..=target.len() {
+        for i in 1..=other.len() {
+            let substitue_cost =
+                if target.chars().nth(j - 1).unwrap() == other.chars().nth(i - 1).unwrap() {
+                    0
+                } else {
+                    1
+                };
+
+            matrix[i][j] = min3(
+                matrix[i - 1][j] + 1,                  //deletion
+                matrix[i][j - 1] + 1,                  //insertion
+                matrix[i - 1][j - 1] + substitue_cost, //substitution
+            );
+        }
+    }
+    matrix[other.len()][target.len()]
+}
+
+fn min3(x: usize, y: usize, z: usize) -> usize {
+    x.min(y.min(z))
+}
+
+fn load_file(path: &Path) -> Result<String, std::io::Error> {
+    let mut file = std::fs::File::open(path)?;
+    let mut result = String::new();
+    file.read_to_string(&mut result)?;
+    Ok(result)
+}
+
+#[cfg(test)]
+mod tests {
+    const DEFAULT_LIMIT: usize = 2;
+    use super::*;
+    use std::path::Path;
+
+    #[test]
+    fn it_works() {
+        let results = search_dir(Path::new("./tests"), "Hello", 2).unwrap();
+        assert_eq!(format!("{}", results[0]), "./tests/hello.txt:1\tHello");
+    }
+
+    #[test]
+    fn fuzzier() {
+        let results = search_dir(Path::new("./tests"), "Hell", 2).unwrap();
+        assert_eq!(format!("{}", results[0]), "./tests/hello.txt:1\tHello");
+
+        let results = search_dir(Path::new("./tests"), "World!", 2).unwrap();
+        assert_eq!(format!("{}", results[0]), "./tests/hello.txt:1\tWorld!");
+    }
+
+    #[test]
+    fn cases() {
+        let results = search_dir(Path::new("./tests"), "hello", 2).unwrap();
+        assert_eq!(format!("{}", results[0]), "./tests/hello.txt:1\tHello");
+    }
+
+    #[test]
+    fn basic_distance() {
+        assert_eq!(
+            levenshtein_distance(&"Hello".to_string(), &"Hello".to_string()),
+            0
+        );
+        assert_eq!(
+            levenshtein_distance(&"Helo".to_string(), &"Hello".to_string()),
+            1
+        );
+        assert_eq!(
+            levenshtein_distance(&"ello".to_string(), &"Hello".to_string()),
+            1
+        );
+        assert_eq!(
+            levenshtein_distance(&"Heo".to_string(), &"Hello".to_string()),
+            2
+        );
+    }
+
+    #[test]
+    fn complex_distance() {
+        assert_eq!(
+            levenshtein_distance(&String::from("kitten"), &String::from("sitting")),
+            3
+        );
+        assert_eq!(
+            levenshtein_distance(&String::from("sunday"), &String::from("saturday")),
+            3
+        );
+        assert_eq!(
+            levenshtein_distance(&String::from("manual"), &String::from("=")),
+            6
+        );
+    }
+}
